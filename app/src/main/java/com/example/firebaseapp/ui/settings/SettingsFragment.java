b3fbc156e5dc6abf6ff14884e9b48155d1b72d5c
Pau Besalú <bepu.cat@gmail.com>
2022-11-05 15:33:39 +0100

    Logout user and UserProfile to be able to change username and save it in the database

--- app/src/main/java/com/example/firebaseapp/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/firebaseapp/ui/settings/SettingsFragment.java
@@ -1,12 +1,26 @@
 package com.example.firebaseapp.ui.settings;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+
+import com.example.firebaseapp.LoginActivity;
+import com.example.firebaseapp.MainActivity;
 import com.example.firebaseapp.R;
+import com.example.firebaseapp.User;
+import com.example.firebaseapp.UserProfileSettingsActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
@@ -14,18 +28,30 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
 
-        Preference button = getPreferenceManager().findPreference("toastMsg");
+        Preference button = getPreferenceManager().findPreference("logoutButton");
+        Preference userProfile = getPreferenceManager().findPreference("userProfile");
 
         if (button != null) {
             button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference arg0) {
-                    Toast.makeText(getActivity(), "Preference button is clicked",
-                            Toast.LENGTH_SHORT).show();
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(getActivity(),LoginActivity.class));
                     return true;
                 }
             });
         }
+
+        if(userProfile != null){
+            userProfile.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(@NonNull Preference preference) {
+                    startActivity(new Intent(getActivity(), UserProfileSettingsActivity.class));
+                    return true;
+                }
+            });
+        }
+
     }
 
 
